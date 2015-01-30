6525e42e447f13d64b163da10f92a04eff799aef
steven <steven.heseltine@gmail.com>
2015-01-30 18:30:58 +1000

    Added support for Resource Owner Password grant type.

--- src/main/java/com/stevenheseltine/api/auth/configuration/OAuthConfiguration.java
+++ src/main/java/com/stevenheseltine/api/auth/configuration/OAuthConfiguration.java
@@ -1,43 +1,56 @@
 package com.stevenheseltine.api.auth.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(new JwtAccessTokenConverter());
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey("secret");
+        return jwtAccessTokenConverter;
     }
 
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws
-            Exception {
-        endpoints.tokenStore(tokenStore());
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        oauthServer.tokenKeyAccess("isAnonymous()");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .accessTokenConverter(accessTokenConverter())
+                .authenticationManager(authenticationManager);
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("crm")
-                .authorities("ROLE_ADMIN")
-                .resourceIds("customer")
+                .withClient("activity-master")
+                .authorizedGrantTypes("implicit")
                 .scopes("read", "write")
-                .authorizedGrantTypes("client_credentials")
-                .secret("password")
+                .redirectUris("http://localhost:8081/api/hello")
                 .and()
-                .withClient("customerportal")
-                .redirectUris("http://customerportal.com/")
-                .resourceIds("customer")
-                .scopes("read")
-                .authorizedGrantTypes("implicit");
+                        // client_credentials requires reference to clientDetailsService on Resource Server!!!
+                .withClient("quick-assist")
+                .authorizedGrantTypes("password")
+                .resourceIds("oauth2-resource")
+                .scopes("read", "write")
+                .authorities("ROLE_API_USERS")
+                .secret("secret");
     }
 }
--- src/main/java/com/stevenheseltine/api/auth/configuration/SecurityConfiguration.java
+++ src/main/java/com/stevenheseltine/api/auth/configuration/SecurityConfiguration.java
@@ -1,8 +1,9 @@
 package com.stevenheseltine.api.auth.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,16 +12,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Configuration
-    protected static class AuthenticationConfiguration extends
-            GlobalAuthenticationConfigurerAdapter {
+    //    @Configuration
+//    protected static class AuthenticationConfiguration extends
+//            GlobalAuthenticationConfigurerAdapter {
+//
+//        @Override
+//        public void init(AuthenticationManagerBuilder auth) throws Exception {
+//            auth.ldapAuthentication().userDnPatterns("uid={0},ou=people")
+//                    .groupSearchBase("ou=groups").contextSource()
+//                    .ldif("classpath:users.ldif");
+//        }
+//    }
+//
 
-        @Override
-        public void init(AuthenticationManagerBuilder auth) throws Exception {
-            auth.ldapAuthentication().userDnPatterns("uid={0},ou=people")
-                    .groupSearchBase("ou=groups").contextSource()
-                    .ldif("classpath:users.ldif");
-        }
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder
+                .ldapAuthentication().userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups").contextSource()
+                .ldif("classpath:users.ldif");
     }
 
     @Override
